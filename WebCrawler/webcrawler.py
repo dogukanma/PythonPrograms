d0fa03cdd762a84574d1aebf7c3c6c365f71877c
dogukanma <blanksh364@gmail.com>
2024-09-24 00:17:08 +0300

    adjustments

--- WebCrawler/webcrawler.py
+++ WebCrawler/webcrawler.py
@@ -3,6 +3,8 @@
 # By using this software, you agree to take full responsibility for your actions and ensure that your usage complies with applicable laws and 
 # regulations. It is recommended to only use this tool on systems for which you have explicit permission.
 
+# This program doesn't work properly
+
 # imports
 import requests
 import sys
@@ -19,7 +21,7 @@ except IndexError:
   exit("Usage: python webcrawler.py your_url")
 
 found_links = []
-social_media = ["facebook", "linkedin", "instagram", "twitter", "youtube", "x.com"]
+invalid_words = ["facebook", "linkedin", "instagram", "twitter", "youtube", "x.com", "javascript", "mailto", "tel:"]
 
 def get_source(sourceURL):
   session = requests.Session()
@@ -29,43 +31,45 @@ def get_source(sourceURL):
   session.mount('http://', adapter)
   response = session.get(sourceURL)
   soup = BeautifulSoup(response.text, "html.parser")
-  # print(soup.find_all('a'))
   return soup
 
+# Error handling on getting source will be added.
 def crawl(sourceURL):
   soup = get_source(sourceURL)
+  # print("check 0")
+  if(soup.find_all('a') == None):
+    print("Error on getting source.")
   for link in soup.find_all('a'):
-    social_flag = False
-    href = link.get('href')
-    if(href and href not in found_links and "#" not in href):
-      if(href.startswith("/")):
-        href = userUrl + "/" + href 
-      for social in social_media:
-        if(social in href):
-          social_flag = True
-          break
-      if(social_flag == True):
-        continue
-      found_links.append(href)
-      print(href)
-    crawl(link)
+    invalid_flag = False
+    problem_href = ""
+    # print("check 1")
+    try:
+      href = link.get('href')
+      problem_href = href
+      # print("check 2")
+      if(href and href not in found_links and "#" not in href):
+        if(href.startswith("/")):
+          # print("check 3")
+          if(href == "/"):
+            # print("check 4")
+            continue
+          href = userUrl + href
+        for word in invalid_words:
+          if(word in href):
+            invalid_flag = True
+            # print("check 5")
+            break
+        if(invalid_flag == True):
+          # print("check 6")
+          continue
+        found_links.append(href)
+        print(href)
+        # print("check 7")
+        if(href not in found_links):
+          crawl(href)
+    except requests.exceptions.ConnectionError:
+      print("Problem on: " + problem_href)
+      continue
+
 
 crawl(userUrl)
-
-# def crawl(url):
-
-
-# def crawl(url):
-#   links = get_source(url)
-#   for link in links.find_all('a'):
-#     link = link.get('href')
-#     if link:
-#       part_link = link.split("#")
-#       link = part_link[0]
-#       if link not in found_links and target_url in link:
-#         found_links.append(link)
-#         print("Found URL --> " + link)
-#         crawl(link)
-
-
-# crawl(target_url)
